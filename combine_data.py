21009c070b2d9c3333a562aefcedeb9ec0803252
suman chapai <sumanchapai@gmail.com>
2026-05-14 13:08:47 +0545

    formatting

--- combine_data.py
+++ combine_data.py
@@ -1,33 +1,35 @@
-import pandas as pd
 import sys
 
-provinces = pd.read_csv('provinces.csv')
-districts = pd.read_csv('districts.csv')
+import pandas as pd
 
-if len(sys.argv) > 1 and sys.argv[1].lower() == 'nepali':
-    local_level = pd.read_csv('local_level_nepali.csv')
+provinces = pd.read_csv("provinces.csv")
+districts = pd.read_csv("districts.csv")
+
+if len(sys.argv) > 1 and sys.argv[1].lower() == "nepali":
+    local_level = pd.read_csv("local_level_nepali.csv")
 else:
-    local_level = pd.read_csv('local_level.csv')
+    local_level = pd.read_csv("local_level.csv")
+
 
 def combine_data(rename_column=True):
-    local_level_with_district = pd.merge(local_level,
-                                         districts,
-                                         on='district_id',
-                                         how='left')
+    local_level_with_district = pd.merge(
+        local_level, districts, on="district_id", how="left"
+    )
     local_level_with_district_and_province = pd.merge(
-        local_level_with_district, provinces, on='province_id', how='left')
+        local_level_with_district, provinces, on="province_id", how="left"
+    )
     local_level_with_district_and_province.drop(
-        ['district_id', 'province_id'],
+        ["district_id", "province_id"],
         axis=1,
         inplace=True,
     )
     if rename_column:
         local_level_with_district_and_province.rename(
             columns={
-                'district_name': 'District',
-                'province_name': 'Province',
-                'local_level_name': 'Local Level',
-                'local_level_id': 'S.N.'
+                "district_name": "District",
+                "province_name": "Province",
+                "local_level_name": "Local Level",
+                "local_level_id": "S.N.",
             },
             inplace=True,
         )
@@ -35,15 +37,15 @@ def combine_data(rename_column=True):
 
 
 def write_to_csv(combined):
-    combined.to_csv('combined.csv', index=False)
+    combined.to_csv("combined.csv", index=False)
 
 
 def write_to_json(combined):
     # Write from dataframe to JSON
-    combined.to_json('combined.json', orient='records')
+    combined.to_json("combined.json", orient="records")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     combined = combine_data(rename_column=False)
     write_to_csv(combined)
     write_to_json(combined)
--- create_provinces.py
+++ create_provinces.py
@@ -1,7 +1,6 @@
 import pandas as pd
 
-
-data_source = 'ward_data.csv'
+data_source = "ward_data.csv"
 
 ward_data = pd.read_csv(data_source)
 
@@ -9,8 +8,8 @@ ward_data = pd.read_csv(data_source)
 # province_id, district_name, location_name, ward_no
 
 # Create provinces
-provinces = ward_data.groupby('province_id')
+provinces = ward_data.groupby("province_id")
 # Write to file.
 for province_index, province_data in provinces:
-    province_id = province_data.iloc[0]['province_id']
+    province_id = province_data.iloc[0]["province_id"]
     print(province_id)
